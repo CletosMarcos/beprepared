8534b9dfb4fad785bb44b8dd57a538b5a7ee00d8
CletosMarcos <cleosmarcos@gmail.com>
2025-01-13 04:30:23 +0200

    initial commit

---
+++ src/test/java/com/cmb/beprepared/BePreparedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmb.beprepared;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BePreparedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
